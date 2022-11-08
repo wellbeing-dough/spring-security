e88f8ed84f849cd05090aad5b9f5b6056c58c305
wellbeing-dough <kdw990202@naver.com>
2022-11-09 02:58:09 +0900

    구글 로그인 준비

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -36,7 +36,10 @@ public class SecurityConfig {
                 .formLogin()
                 .loginPage("/loginForm")
                 .loginProcessingUrl("/login")  //login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해준다
-                .defaultSuccessUrl("/");    //login 완료되면 메인페이지로
+                .defaultSuccessUrl("/")    //login 완료되면 메인페이지로
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm");   //구글 로그인이 완료된 후에 후처리가 필요
         return http.build();
     }
 }
